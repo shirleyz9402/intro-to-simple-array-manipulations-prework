0d4239ad01b234ebc023eb9d08a5c8b1b672b817
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-21 22:53:13 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_push(array, string)
+def using_push(array,string)
   array.push(string)
 end 
 
@@ -26,6 +26,6 @@ def using_concat(arr1,arr2)
   arr1.concat(arr2)
 end 
 
-def using_insert(array, element)
-  array.insert(3, element)
+def using_insert(array,element)
+  array.insert(3,element)
 end 
